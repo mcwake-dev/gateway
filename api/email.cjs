78d4cbb6c7a069dd39df864536b577e3a9e86f64
Matthew C Wake <mcwake-dev@outlook.com>
2023-01-03 04:00:51 +0000

    Switched to CommonJS to see if this will deploy correctly on Deno Deploy

--- api/email.cjs
+++ api/email.cjs
@@ -1,10 +1,12 @@
-import sgMail from "@sendgrid/mail";
+const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export async function sendMail(to, from, subject, text, html) {
+async function sendMail(to, from, subject, text, html) {
     const result = await sgMail.send({ to, from, subject, text, html });
 
     return result;
 }
+
+module.exports = { sendMail }
 
