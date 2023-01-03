f9f04d359a7751f1583e641c63307f4536dde5e0
Matthew C Wake <mcwake-dev@outlook.com>
2023-01-03 04:05:15 +0000

    Updated to remove Vercel-specific alterations

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import expressWinston from "express-winston";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 import { config } from "./config.js";
-import { sendMail } from "./email.cjs";
+import { sendMail } from "./email.js";
 
 const app = express();
 const redisStore = connectRedis(session);
--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-import { app } from "./index.js";
+import { app } from "./app.js";
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
